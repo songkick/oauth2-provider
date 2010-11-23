e3015fc97de5cc634dbd30c9410194c438d8ed91
James Coglan <jcoglan@gmail.com>
2010-11-23 12:14:47 +0000

    Spec: clients should delete their authorizations when they are deleted.

--- spec/oauth2/model/client_spec.rb
+++ spec/oauth2/model/client_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe OAuth2::Model::Client do
+  before do
+    @client = Factory(:client)
+    @owner  = Factory(:owner)
+    Factory(:authorization, :client => @client, :owner => @owner)
+  end
   
+  it "destroys its authorizations on destroy" do
+    @client.destroy
+    OAuth2::Model::Authorization.count.should be_zero
+  end
 end
 
