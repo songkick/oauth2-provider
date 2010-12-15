51cc6e7c780bc60cbc9fde45435e8f384ffbf5a3
James Coglan <jcoglan@gmail.com>
2010-12-15 14:36:10 +0000

    Add a helper method for getting authorizations by access token, hiding the hashing details.

--- lib/oauth2/model.rb
+++ lib/oauth2/model.rb
@@ -8,6 +8,10 @@ module OAuth2
     autoload :Authorization,  ROOT + '/oauth2/model/authorization'
     autoload :Client,         ROOT + '/oauth2/model/client'
     autoload :Schema,         ROOT + '/oauth2/model/schema'
+    
+    def self.find_access_token(access_token)
+      Authorization.find_by_access_token_hash(OAuth2.hashify(access_token))
+    end
   end
 end
 
