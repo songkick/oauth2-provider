4b46d640b4c80fa218808d6237e5fe4f5a4f9234
Brandon Aaron <brandon.aaron@gmail.com>
2010-12-14 19:49:46 -0600

    Separate out the ResourceOwner into AuthorizationOwner and ClientOwner so that the client application can be registered to other owners (e.g. A Company owns the Client and the User owns the Authorization)

--- lib/oauth2/model.rb
+++ lib/oauth2/model.rb
@@ -2,12 +2,13 @@ require 'active_record'
 
 module OAuth2
   module Model
-    autoload :BelongsToOwner, ROOT + '/oauth2/model/belongs_to_owner'
-    autoload :ResourceOwner,  ROOT + '/oauth2/model/resource_owner'
-    autoload :Hashing,        ROOT + '/oauth2/model/hashing'
-    autoload :Authorization,  ROOT + '/oauth2/model/authorization'
-    autoload :Client,         ROOT + '/oauth2/model/client'
-    autoload :Schema,         ROOT + '/oauth2/model/schema'
+    autoload :BelongsToOwner,     ROOT + '/oauth2/model/belongs_to_owner'
+    autoload :ClientOwner,        ROOT + '/oauth2/model/client_owner'
+    autoload :AuthorizationOwner, ROOT + '/oauth2/model/authorization_owner'
+    autoload :Hashing,            ROOT + '/oauth2/model/hashing'
+    autoload :Authorization,      ROOT + '/oauth2/model/authorization'
+    autoload :Client,             ROOT + '/oauth2/model/client'
+    autoload :Schema,             ROOT + '/oauth2/model/schema'
   end
 end
 
