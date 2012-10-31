e2d7b34190725de90aca49ec651eaeda8bd26388
James Coglan <jcoglan@gmail.com>
2012-10-31 14:54:01 +0000

    Schema.up should really be called Schema.migrate -- add an alias.

--- lib/songkick/oauth2/schema.rb
+++ lib/songkick/oauth2/schema.rb
@@ -2,9 +2,12 @@ module Songkick
   module OAuth2
     
     class Schema
-      def self.up
+      def self.migrate
         ActiveRecord::Base.logger ||= Logger.new(StringIO.new)
         ActiveRecord::Migrator.up(migrations_path)
+      end
+      class << self
+        alias :up :migrate
       end
       
       def self.migrations_path
