a3e50fde70be272e3daec16013fce939bf53801e
Hari Gopal <harigopal1@gmail.com>
2013-07-10 10:54:38 +0530

    Fixing wording of contexts in Helpers module spec.

--- spec/songkick/oauth2/model/helpers_spec.rb
+++ spec/songkick/oauth2/model/helpers_spec.rb
@@ -10,13 +10,13 @@ describe Songkick::OAuth2::Model::Helpers do
       3.times { Factory(:client, :owner => owner) }
     end
 
-    context 'when conditions are passed' do
+    context 'when conditions are not passed' do
       it 'returns count of total rows' do
         subject.count(owner.oauth2_clients).should == 3
       end
     end
 
-    context 'when conditions are not passed' do
+    context 'when conditions are passed' do
       it 'returns count of rows satisfying supplied conditions' do
         subject.count(Songkick::OAuth2::Model::Client, :client_id => Songkick::OAuth2::Model::Client.first.client_id).should == 1
       end
