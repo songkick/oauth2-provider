2d54714b823fdc9e1494f4918163bde54f4f220c
Vincent Paca <vincent.paca@gmail.com>
2015-05-15 14:32:01 +0800

    Fix

--- lib/songkick/oauth2/redirect_uri_matcher.rb
+++ lib/songkick/oauth2/redirect_uri_matcher.rb
@@ -2,12 +2,8 @@ module Songkick
   module OAuth2
     class RedirectURIMatcher
 
-      class << self
-
-        def match?(client_uri, param_uri)
-          Globber.new(client_uri) =~ param_uri
-        end
-
+      def self.match?(client_uri, param_uri)
+        Globber.new(client_uri) =~ param_uri
       end
 
     end
